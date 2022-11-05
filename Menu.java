03c20c6ef1c22a7e30a47e9cd03fb1e171d10d32
auliaa22 <116439777+auliaa22@users.noreply.github.com>
2022-11-05 13:39:39 +0700

    Add files via upload
    Modul 1

---
+++ Menu.java
@@ -0,0 +1,22 @@
+public class Menu {
+
+    String Name;
+    String Category;
+    String Price;
+
+    public void setName (String Name) {
+        this.Name = Name;
+    }
+
+    public void SetCategory (String Category) {
+        this.Category = Category;
+    }
+
+    public void SetPrice (String Price) {
+        this.Price = Price;
+    }
+
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
---
+++ Server.java
@@ -0,0 +1,22 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        
+
+
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
